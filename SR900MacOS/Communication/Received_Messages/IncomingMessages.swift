98a1fae6ae645b473e1e24696942230447effbbd
dflohr <35896994+dflohr@users.noreply.github.com>
2025-11-10 16:25:24 -0500

    Note to Nisarg in IncomingMessages.swift

---
+++ SR900MacOS/Communication/Received_Messages/IncomingMessages.swift
@@ -0,0 +1,32 @@
+//
+//  IncomingMessages.swift
+//  SR900MacOS
+//
+//  Created by Daniel Flohr on 11/10/25.
+//
+/*
+ /*
+  
+Nisarg:
+  
+  First step here is to:
+  
+  1. comment out Lines 147-152 from BLEManager.  These lines display the incoming byte stream stored in "lastReceivedBytes"  on the textbox on the main panel.
+  2. Add that function here so that in this file, we can access lastReceivedBytes   and display on the textbox on the main panel.
+  
+  Hold the message for 1 second then delete it.
+  
+  Important step ahead of parsing tasks.
+
+
+
+
+
+
+
+
+
+
+
+
+*/
